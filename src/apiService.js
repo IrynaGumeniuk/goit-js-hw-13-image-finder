56f91b00b9695d8c6579858ff1a5fae011045984
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2021-08-26 21:05:40 +0200

    api service, js doc and css styles

---
+++ src/apiService.js
@@ -0,0 +1,38 @@
+export default class ApiService {
+
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchImages() {
+        const API_KEY = '23089090-b881069149d08b76d7d8b0f8f';
+        const BASE_URL = 'https://pixabay.com/api';
+        return fetch(
+            `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+        )
+            .then(r => r.json())
+            .then(({ hits }) => {
+                this.incrementPage();
+
+                return hits;
+            });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,62 +1,79 @@
-import renderCountries from './templates/list-countries.hbs'
-import renderCountry from './templates/one-country.hbs';
-import debounce from 'lodash.debounce';
-import fetchCountries from './fetchCountries';
+import cardsImgsTpl from './templates/cardTpl.hbs';
+import ApiService from './apiService';
+import LoadMoreBtn from './loadmore';
 
-
-import { info, error } from '@pnotify/core';
+import { inform, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-let foundedCountry = '';
 
 const refs = {
-    container: document.querySelector('.js-countries'),
-    input: document.querySelector('.countryInput')
+    searchForm: document.querySelector('.search-form'),
+    imageContainer: document.querySelector('.gallery'),
 };
 
-refs.input.addEventListener('input', debounce(() => {
-    onSearch();
-}, 500));
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
 
-function onSearch() {
-    resetSearch();
-    foundedCountry = refs.input.value;
-    fetchCountries(foundedCountry)
-        .then(contentOutput)
-        .catch(err => console.log(err));
-}
+const apiService = new ApiService();
 
-function resetSearch() {
-    refs.container.innerHTML = '';
-}
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
-// функция отрисовки контента
+function onSearch(e) {
+    e.preventDefault();
 
-function contentOutput(countries) {
-    if (countries.length === 1) {
-        resetSearch();
-        markupContries(renderCountry, countries);
-    } else if (countries.length > 1 && countries.length <= 10) {
-        resetSearch();
-        markupContries(renderCountries, countries);
-    } else if (countries.length > 10) {
-        resultMessage(
-            error,
-            'To many matches found. Please enter a more specific query!',
-        );
-    } else {
-        resultMessage(info, 'No matches found!');
+    apiService.query = e.currentTarget.elements.query.value;
+
+    if (apiService.query === '') {
+        return inform({
+            text: 'Enter the value!',
+            delay: 1500,
+            closerHover: true,
+        });
     }
+
+    loadMoreBtn.show();
+    apiService.resetPage();
+    clearImagesContainer();
+    fetchImages();
 };
 
-function resultMessage(typeInfo, textInfo) {
-    typeInfo({
-        text: `${textInfo}`,
-        delay: 1000,
-        closerHover: true,
+function fetchImages() {
+    loadMoreBtn.disable();
+
+    return apiService.fetchImages().then(images => {
+        appendImagesMarkup(images);
+        loadMoreBtn.enable();
+        if (images.length === 0) {
+            loadMoreBtn.hide();
+            error({
+                text: 'No matches found!',
+                delay: 1500,
+                closerHover: true,
+            });
+        }
     });
 }
 
-function markupContries(tpl, countries) {
-    refs.container.insertAdjacentHTML('beforeend', tpl(countries));
+function appendImagesMarkup(images) {
+    refs.imageContainer.insertAdjacentHTML('beforeend', cardsImgsTpl(images));
+}
+
+function clearImagesContainer() {
+    refs.imageContainer.innerHTML = '';
+}
+
+function onLoadMore() {
+    fetchImages()
+        .then(
+            setTimeout(() => {
+                window.scrollBy({
+                    top: document.documentElement.clientHeight - 100,
+                    behavior: 'smooth',
+                });
+            }, 1500),
+        )
+        .catch(err => console.log(err));
 }
